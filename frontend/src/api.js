916ca43c3bd39a4781095ed99e808f958390ee2c
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-12-05 03:04:19 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,18 +1,17 @@
-// src/api.js
 import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_URL || "/api";
 
-function getToken(){
+function getToken() {
   return localStorage.getItem("token");
 }
 
-export function apiClient() {
-  const instance = axios.create({
+export function client() {
+  const inst = axios.create({
     baseURL: API_BASE,
     headers: { "Content-Type": "application/json" }
   });
   const token = getToken();
-  if (token) instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  return instance;
+  if (token) inst.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  return inst;
 }
