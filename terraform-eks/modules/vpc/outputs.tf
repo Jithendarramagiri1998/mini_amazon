e28ada5d7624f58ada0dfe948c391f6fd997184d
Jithendarramagiri <138892392+Jithendarramagiri1998@users.noreply.github.com>
2025-12-06 02:20:19 +0530

    Update outputs.tf

--- terraform-eks/modules/vpc/outputs.tf
+++ terraform-eks/modules/vpc/outputs.tf
@@ -1,11 +1,11 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.aws_vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.aws_vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.aws_vpc.public_subnets
 }
